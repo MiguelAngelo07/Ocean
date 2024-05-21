614176da6bdc82fa87ffcca870ef9c0bb92e37e6
Miguelsouza161220 <156853644+MiguelAngelo07@users.noreply.github.com>
2024-05-21 16:09:23 -0300

    Update python_aula_1.py
    att

--- python/python_aula_1.py
+++ python/python_aula_1.py
@@ -14,19 +14,20 @@
     # print('Idoso')
 
 
-#num = int(input('Entre com um número: '))
-#if num < 0:
-#    print(f'{num}, Valor lido é negativo')
-# else:
-#    print(f'{num}, Valor é positivo.')
+num = int(input('Entre com um número: '))
+if num < 0:
+    print(f'{num}, Valor lido é negativo')
+else:
+    print(f'{num}, Valor é positivo.')
 
 num = int(input('Entre com um número: '))
-#if num % 2 == 0:
-#    print(f'{num} é par')
-#else:
-#    print(f'{num} é')
+if num % 2 == 0:
+    print(f'{num} é par')
+else:
+    print(f'{num} é ímpar')
 
 if (num % 2 == 0) and (num % 3 == 0):
     print (f'{num} é divisível por 2 e 3')
 else:
     print(f'{num} não é divisível por 2 e 3')
+    
