eff54f1c256072649620194acbacecedd0050a3a
Miguelsouza161220 <156853644+MiguelAngelo07@users.noreply.github.com>
2024-05-21 16:01:22 -0300

    Update python_aula_1.py

--- python/python_aula_1.py
+++ python/python_aula_1.py
@@ -1,14 +1,32 @@
-print("Hello World")
+#print("Hello World")
 
-nome = input("Qual o seu nome?")
-print(f'Olá, {nome}, seja bem-vindo')
+# nome = input("Qual o seu nome? ")
+# print(f'Olá, {nome}, seja bem-vindo')
 
-idade = int(input('Qual a sua idade?'))
-if idade < 13:
-    print('Criança')
-elif idade < 18:
-    print ('Adolescente')
-elif idade < 60:
-    print('Adulto')
+# idade = int(input('Qual a sua idade? '))
+# if idade < 13:
+    # print('Criança')
+#elif idade < 18:
+    # print ('Adolescente')
+# elif idade < 60:
+    # print('Adulto')
+# else:
+    # print('Idoso')
+
+
+#num = int(input('Entre com um número: '))
+#if num < 0:
+#    print(f'{num}, Valor lido é negativo')
+# else:
+#    print(f'{num}, Valor é positivo.')
+
+num = int(input('Entre com um número: '))
+#if num % 2 == 0:
+#    print(f'{num} é par')
+#else:
+#    print(f'{num} é')
+
+if (num % 2 == 0) and (num % 3 == 0):
+    print (f'{num} é divisível por 2 e 3')
 else:
-    print('Idoso')
+    print(f'{num} não é divisível por 2 e 3')
